fe2510a94171ce79581d703b1219ba6b9265053c
lea927 <lxthym@icloud.com>
2021-07-27 23:39:33 +0800

    Adds category controller with default CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'category/index'
+  get 'category/show'
+  get 'category/new'
+  get 'category/edit'
+  get 'category/create'
+  get 'category/update'
+  get 'category/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
