dec8b34fe9bfa7b0011bc976bb1a10344e193a36
lea927 <lxthym@icloud.com>
2021-07-29 11:48:34 +0800

    Runs test on browser

--- Gemfile
+++ Gemfile
@@ -31,7 +31,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
-
+ # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '>= 3.26'
+  gem 'selenium-webdriver'
 end
 
 group :development do
@@ -46,9 +48,7 @@ group :development do
 end
 
 group :test do
-  # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 3.26'
-  gem 'selenium-webdriver'
+ 
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
 end
--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "CreatingCategories", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+# RSpec.describe "CreatingCategories", type: :system do
+#   before do
+#     driven_by(:rack_test)
+#     # driven_by(:selenium_chrome_headless)
+
+#   end
+
+RSpec.describe "CreatingCategories", driver: :selenium_chrome, js: true do
 
   it "saves and displays the resulting category" do
     # Visiting new category page
     visit '/categories/new'
+    sleep(3)
 
     # Entering name into form field
     fill_in 'Name', with: 'School'
+    sleep(3)
 
     # Clicking on add category button 
     click_on 'Create Category'
+    sleep(3)
 
     # Expecting category to appear on the screen
     expect(page).to have_content('School')
+    sleep(3)
 
     # Confirming category was saved in the database
     category = Category.order("id").last
     expect(category.name).to eq('School')
+    sleep(3)
 
   end
 end
