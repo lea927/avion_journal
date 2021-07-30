9e0af8db539a066c89ad17b5098cd57f9ac322a1
lea927 <lxthym@icloud.com>
2021-07-30 22:58:09 +0800

    Updates editing categories specs

--- spec/system/editing_categories_spec.rb
+++ spec/system/editing_categories_spec.rb
@@ -6,19 +6,48 @@ RSpec.describe "EditingCategories", type: :system do
   end
 
   it "edits category and displays index page" do
+    # destroys existing category to prevent ambigious error
+    Category.destroy_all
+    # Adds category 
+    school = Category.create!(name: "School")
 
-    # Visiting edit category page
-    visit '/categories/:id/edit'
+    # Visiting index to display all categories
+    visit categories_path
+
+    # Checks if on the index path
+    expect(page).to have_current_path categories_path
 
     # Expecting category to appear on the screen
-    category = Category.order("id").last
-    expect(category.name).to eq('School')
+    expect(page).to have_content('School')
+    expect(page).to have_content('Show')
 
+    # Clicks Show link
+    click_link 'Show'
+
+    # Checks if on the show path
+    expect(page).to have_current_path category_path(id: school.id)
+
+    # Expecting category to appear on the screen under show path
+    expect(page).to have_content('School')
+    expect(page).to have_content('Edit')
+
+    # Clicks Edit link
+    click_link 'Edit'
+
+    # Checks if on the edit path
+    expect(page).to have_current_path edit_category_path(id: school.id)
+
+    # Expecting category to appear on the screen under edit path
+    expect(page).to have_content('Name')
+    
     # Filling in with new data
     fill_in 'Name', with: 'School - Avion'
 
     # Clicking on update category button
     click_on 'Update Category'
+
+    # Checks if on the show path
+    expect(page).to have_current_path category_path(id: school.id)
 
     # Expecting edited category to appear on screen
     expect(page).to have_content('School - Avion')
